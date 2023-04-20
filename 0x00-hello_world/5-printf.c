24bd3f1e88651b5a1206ee64e111f4c8ec629bf6
mennaelzahar <menna@aucegypt.edu>
2023-04-20 13:18:41 -0700

    trying printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'A program to practise the "printf" function'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
