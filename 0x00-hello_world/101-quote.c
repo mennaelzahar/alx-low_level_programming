ed113e9a86d2eec66d3f2c26da84fdfd27529cc1
mennaelzahar <menna@aucegypt.edu>
2023-04-20 18:26:10 -0700

    new commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
+#include <string.h>
 /**
  * main - Entry point
  * Description: 'printing to standard error'
  * Return: Always 0 (Success)
  */
 
-int main(void) {
-    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, msg, sizeof(msg) - 1);
-    return 1;
+int main(void)
+{
+char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, msg, sizeof(msg) - 1);
+return (1);
 }
