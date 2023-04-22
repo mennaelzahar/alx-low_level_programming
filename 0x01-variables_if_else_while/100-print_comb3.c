3612506d351bb14abb46850dc1486d26c6ff8a7e
mennaelzahar <menna@aucegypt.edu>
2023-04-21 18:27:50 -0700

    new commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -21,11 +21,8 @@ int main(void)
 			{
 				putchar((char)ch);
 				putchar((char)x);
-				if (ch < 56 && x < 57)
-				{
-					putchar(',');
-					putchar((char)SPACE);
-				}
+				putchar(',');
+				putchar((char)SPACE);
 			}
 		}
 	}
