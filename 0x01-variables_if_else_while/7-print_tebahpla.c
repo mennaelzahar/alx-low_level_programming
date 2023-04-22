50fe466e43c2840691e3f08ed226c35efa84a68a
mennaelzahar <menna@aucegypt.edu>
2023-04-21 17:57:59 -0700

    new commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'decide if a number is positive or negative'
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z' ; ch >= 'a' ; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
