84a498b98246c7099ab1f624a7c417c50547f838
mennaelzahar <menna@aucegypt.edu>
2023-04-21 18:16:58 -0700

    new commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'decide if a number is positive or negative'
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int ch;
+	int x;
+	int SPACE = 32;
+
+	for (ch = 48 ; ch <= 57 ; ch++)
+	{
+		putchar((char)ch);
+		for (x = 48 ; x <= 57 ; x++)
+			if (ch != x)
+				putchar((char)x);
+		if (ch < 57)
+		{
+			putchar(',');
+			putchar((char)SPACE);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
