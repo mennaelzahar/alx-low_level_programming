5d7795d0028230ef1ab9ff252096bbc617b2b8b6
mennaelzahar <menna@aucegypt.edu>
2023-04-21 17:56:32 -0700

    new commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 48 ; ch <= 57 ; ch++)
+	{
+		putchar((char)ch);
+	}
+	putchar('\n');
+	return (0);
+}
