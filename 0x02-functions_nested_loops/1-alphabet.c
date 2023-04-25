f57f191524f294d10144e9fd54eee8026181e26e
mennaelzahar <menna@aucegypt.edu>
2023-04-25 09:30:26 -0700

    added void print_alphabet(void);

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,11 @@
+#include "main.h"
+
 void print_alphabet(void)
 {
-        for (char ch = 'a'; ch <= 'z'; ch++)
+	char ch;
+        for (ch = 'a'; ch <= 'z'; ch++)
         {
                 _putchar(ch);
         }
         _putchar('\n');
-        return (0);
 }
