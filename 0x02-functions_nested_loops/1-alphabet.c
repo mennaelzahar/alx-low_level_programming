a3b886024170afccc8e4e32833d2c1ba0d46f88d
mennaelzahar <menna@aucegypt.edu>
2023-04-25 09:53:23 -0700

    edited

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
+/**
+ * print_alphabet: prints alphabet
+ * Return: none.
+ */
+
 void print_alphabet(void)
 {
 	char ch;
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-                _putchar(ch);
-        }
-        _putchar('\n');
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
 }
