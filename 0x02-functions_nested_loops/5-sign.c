5052b0c0b6365122388637607eb4f027eccb311f
mennaelzahar <menna@aucegypt.edu>
2023-04-25 17:55:51 -0700

    checks num sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,25 +13,16 @@ int print_sign(int n)
 	if (n == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		_putchar('0');
 		return (0);
 	}
 	else if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
-		_putchar('1');
 		return (1);
 	}
 	else
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
-		_putchar('/');
 		return (-1);
 	}
 }
