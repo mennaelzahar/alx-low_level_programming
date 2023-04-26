be62ed5dfeb11bfffe7ef9e8406151eb9d5cc450
mennaelzahar <menna@aucegypt.edu>
2023-04-25 17:13:53 -0700

    fixed betty doc

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet: prints alphabet
- * Return: none.
+ * print_alphabet - prints the alphabet in lowercase
+ * Description: prints the alphabet in lowercase using for loop
  */
 
 void print_alphabet(void)
