35cbdb2158c2ac17366584ad0bdad3dfae79eb2c
mennaelzahar <menna@aucegypt.edu>
2023-04-25 18:01:19 -0700

    abs val

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs(int num)- prints abs value
+ * @num: a int  member
+ * Return: int value
+ * Description: returns abs value
+ */
+int _abs(int num)
+{
+	return (abs(num));
+}
