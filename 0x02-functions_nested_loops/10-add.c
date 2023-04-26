c7c78d3e798654e794019e63d071569dbd96c309
mennaelzahar <menna@aucegypt.edu>
2023-04-25 20:32:25 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+#include <limits.h>
+
+/**
+ * add- adds two nums
+ * @a: num 1
+ * @b: num 2
+ * Return: num
+ * Description: returns last digit of num
+ */
+int add(int a, int b)
+{
+	return (a + b);
+
+}
