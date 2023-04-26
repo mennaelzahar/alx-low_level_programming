c432165b9acdf768944cde25f3ea5ef97e4fa12b
mennaelzahar <menna@aucegypt.edu>
2023-04-25 17:20:17 -0700

    added func to print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase x10
+ * Description: prints the alphabet in lowercase using for loop x10
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+	}
+	_putchar('\n');
+}
