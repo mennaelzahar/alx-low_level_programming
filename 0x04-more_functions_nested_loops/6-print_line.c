add1d6e74af2fe80cd160c2ad83801408b2d85b8
mennaelzahar <menna@aucegypt.edu>
2023-04-27 17:57:25 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_line - check the code.
+ * @n: int number
+ * Description: 'print numbers'
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
