80f5ce489468dce74f6386b8a66fd359a6f032c4
mennaelzahar <menna@aucegypt.edu>
2023-04-27 17:11:05 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * mul - check the code.
+ *@a: integer
+ *@b: integer
+ * Return: an int
+ * Description: 'check if digit'
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
