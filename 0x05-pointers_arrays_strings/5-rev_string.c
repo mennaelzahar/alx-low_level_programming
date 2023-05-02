6f93737783be620f27fa4f2d4043048bab723650
mennaelzahar <menna@aucegypt.edu>
2023-05-02 16:31:36 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * rev_string - check the code.
+ * @s: pointer to char
+ * Description: 'return string size'
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int size = strlen(s);
+
+	for (i = 0; i < size / 2; i++)
+	{
+		char tmp = s[i];
+
+		s[i] = s[size - i - 1];
+		s[size - i - 1] = tmp;
+    }
+}
