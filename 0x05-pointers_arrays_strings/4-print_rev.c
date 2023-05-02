7baa2391a27be616601bb2ad6cf959a5002a330e
mennaelzahar <menna@aucegypt.edu>
2023-05-02 16:13:49 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_rev - check the code.
+ * @s: pointer to char
+ * Description: 'return string size'
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int size = strlen(s);
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+}
