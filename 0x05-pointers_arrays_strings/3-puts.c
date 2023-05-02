6c91238d637863a74499d28b990176afbddb980d
mennaelzahar <menna@aucegypt.edu>
2023-05-02 15:57:52 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _puts - check the code.
+ * @str: pointer to char
+ * Description: 'return string size'
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+	puts("\n");
+}
