f7bc8f23e7cb0b41e20f3664d56580673dcc663e
mennaelzahar <menna@aucegypt.edu>
2023-05-02 15:37:59 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strlen- check the code.
+ *@s: pointer to char
+ * Description: 'reset_to_98'
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
