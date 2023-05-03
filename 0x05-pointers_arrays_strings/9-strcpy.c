b6204a9fb59220db65f85f569ee28fc6bd436685
mennaelzahar <menna@aucegypt.edu>
2023-05-02 17:57:46 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcpy - check the code.
+ * @src: pointer to char
+ * @dest: pointer to char
+ * Return: char *
+ * Description: 'return string size'
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
