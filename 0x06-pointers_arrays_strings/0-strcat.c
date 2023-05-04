14a762093d50dbeb5469a97263d01ff8eb6bd51f
mennaelzahar <menna@aucegypt.edu>
2023-05-04 12:15:38 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - check the code
+ * @dest: string
+ * @src: string
+ * Return: char *.
+ * Description: string concatination
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+More C Practice forPointers, Arrays, and Strings
