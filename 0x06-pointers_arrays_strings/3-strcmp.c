853a72e3a1d923dc1b155f54b9b81e27ecb263dc
mennaelzahar <menna@aucegypt.edu>
2023-05-04 12:55:51 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcmp - check the code
+ * @s1: string
+ * @s2: string
+ * Return: int
+ * Description: string concatination
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
