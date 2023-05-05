664d91e52a12e33a5acf0869faef0811f54bf545
mennaelzahar <menna@aucegypt.edu>
2023-05-04 17:16:38 -0700

    Task 6.0

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,40 @@
-#include "main.h"
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 /**
- * cap_string - check the code
- * @str: pointer to char
- * Return: char *
- * Description: string concatination
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to modify.
+ *
+ * Return: A pointer to the modified string.
  */
 char *cap_string(char *str)
 {
-	int i;
-	int len = strlen(str);
+	int i, j;
 	int cap_next = 1;
+	int len = strlen(str);
+	char separators[] = " \t\n,;.!?\"(){}";
+	int num_separators = strlen(separators);
 
 	for (i = 0; i < len; i++)
 	{
-		if (isspace(str[i]) || ispunct(str[i])) 
+		for (j = 0; j < num_separators; j++)
 		{
-			cap_next = 1;
+			if (str[i] == separators[j])
+			{
+				cap_next = 1;
+				break;
+			}
 		}
-		else if (cap_next)
+		if (j == num_separators) 
 		{
-			str[i] = toupper(str[i]);
-			cap_next = 0;
+			if (cap_next)
+			{
+				str[i] = toupper(str[i]);
+				cap_next = 0;
+			}
 		}
 	}
 	return (str);
 }
+
