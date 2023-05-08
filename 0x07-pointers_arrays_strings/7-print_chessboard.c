57a2eef2e063a49af5f01cc7327f6651c14b7fcc
mennaelzahar <menna@aucegypt.edu>
2023-05-08 09:16:40 -0700

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_chessboard - check the code
+ * @a: char * array
+ * Description: setting to 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+			putchar(' ');
+		}
+		putchar('\n');
+
+	}
+}
