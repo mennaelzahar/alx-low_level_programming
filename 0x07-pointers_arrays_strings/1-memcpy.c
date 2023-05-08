ad40c63e2cf5124c7781421d3e4d2e48588cba09
mennaelzahar <menna@aucegypt.edu>
2023-05-08 08:31:09 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _memcpy - check the code
+ * @dest: string
+ * @src: string
+ * @n: int
+ * Return: char *.
+ * Description: setting to 0
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (memcpy(s, b, n));
+}
