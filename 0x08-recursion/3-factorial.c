994932b303c94c172582940435b4b523101831e9
mennaelzahar <menna@aucegypt.edu>
2023-05-10 12:21:37 -0700

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* factorial - check the code
+*@n: int
+* Return: int
+* Description: print a string
+*/
+int factorial(int n)
+{
+	if (n == 0)
+		return 1;
+	if (n < 0)
+		return -1;
+	return (n * factorial(n - 1));
+}
