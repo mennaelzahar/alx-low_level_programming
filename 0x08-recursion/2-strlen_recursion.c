7b0df035bcee57fc2a546647f1b5364e1cf6eccf
mennaelzahar <menna@aucegypt.edu>
2023-05-10 12:15:57 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strlen_recursion - check the code
+*@s: char *
+* Return: int
+* Description: print a string
+*/
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
