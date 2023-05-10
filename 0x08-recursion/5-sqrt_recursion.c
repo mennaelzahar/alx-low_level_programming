f32a05274d98ea91dce9fb06d340520d2a4a79c8
mennaelzahar <menna@aucegypt.edu>
2023-05-10 12:59:22 -0700

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+#include <math.h>
+
+/**
+* _sqrt_recursion - check the code
+*@n: int
+* Return: int
+* Description: print a string
+*/
+
+int helper(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (helper(n, i + 1));
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (helper(n, 0));
+}
