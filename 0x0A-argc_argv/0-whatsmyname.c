d49a4f6e7500900d1012a40645c49ff446a8a2f5
mennaelzahar <menna@aucegypt.edu>
2023-05-13 15:09:13 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *@argc: int
+ *@argv: char **
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	if (argc)
+		printf("%s\n", *argv);
+	return (0);
+}
