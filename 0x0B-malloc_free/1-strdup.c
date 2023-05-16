9944535900bba67959007cbbe41498391e3db979
mennaelzahar <menna@aucegypt.edu>
2023-05-16 16:05:01 -0700

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,17 +13,17 @@ char *_strdup(char *str)
 	size_t length;
 
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 
 	length = strlen(str);
 
 	duplicate = malloc((length + 1) * sizeof(char));
 
 	if (duplicate == NULL)
-		return NULL;
+		return (NULL);
 
 	strcpy(duplicate, str);
 
-	return duplicate;
+	return (duplicate);
 }
 
